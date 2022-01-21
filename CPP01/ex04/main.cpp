310b7a52fa789f0e29b8208e070e3e49e973bb4e
Manon Berne <mberne@student.42lyon.fr>
2022-01-21 15:33:46 +0100

    fix convertion for linux CPP01/ex04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -3,18 +3,23 @@
 
 int main(int ac, char **av)
 {
+	if (ac != 4)
+	{
+		std::cout << "Wrong number of parameters" << std::endl;
+		return 1;
+	}
 	std::string file = av[1];
 	std::string s1 = av[2];
 	std::string s2 = av[3];
-	if (ac != 4 || file.empty() || s1.empty() || s2.empty() || !s1.compare(s2))
+	if (file.empty() || s1.empty() || s2.empty() || !s1.compare(s2))
 	{
 		std::cout << "Wrong parameters" << std::endl;
 		return 1;
 	}
 	std::ifstream infile;
 	std::ofstream outfile;
-	infile.open(file);
-	outfile.open(file.append(".replace"), std::ios::trunc);
+	infile.open(file.c_str());
+	outfile.open(file.append(".replace").c_str(), std::ios::trunc);
 	if (!infile.is_open() || !outfile.is_open())
 	{
 		std::cout << "File/Open error" << std::endl;
