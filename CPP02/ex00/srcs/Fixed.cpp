99b530ab7a55f2d6cd9259d68142a6a72ced7774
Manon Berne <mberne@student.42lyon.fr>
2022-01-25 12:51:50 +0100

    CPP02/ex00 done

---
+++ CPP02/ex00/srcs/Fixed.cpp
@@ -0,0 +1,37 @@
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _fixed_value(0)
+{
+	std::cout << "Default constructor called." << std::endl;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+	std::cout << "Copy constructor called." << std::endl;
+	*this = src;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called." << std::endl;
+}
+
+Fixed	&Fixed::operator=(Fixed const &rhs)
+{
+	std::cout << "Assignation operator called." << std::endl;
+	this->_fixed_value = rhs.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called." << std::endl;
+	return (_fixed_value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called." << std::endl;
+	_fixed_value = raw;
+}
