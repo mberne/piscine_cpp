7c05ba7324bbf7458d6c3459a8ceeac9caec653a
Manon Berne <mberne@student.42lyon.fr>
2022-02-22 10:32:48 +0100

    CPP08/ex02 done

--- CPP08/ex02/srcs/main.cpp
+++ CPP08/ex02/srcs/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 #include "MutantStack.hpp"
 
 int main()
@@ -43,45 +44,36 @@ int main()
 		}
 	}
 
-	// {	
-	// 	std::cout << std::endl << "_____Their main._____" << std::endl;
+	{	
+		std::cout << std::endl << "_____Their main with list._____" << std::endl;
 
-	// 	MutantStack<int> mstack;
+		std::list<int> mstack;
 
-	// 	mstack.push_back(5);
-	// 	mstack.push_back(17);
+		mstack.push_back(5);
+		mstack.push_back(17);
 
-	// 	std::cout << mstack.front() << std::endl;
+		std::cout << mstack.back() << std::endl;
 
-	// 	mstack.pop_back();
+		mstack.pop_back();
 
-	// 	std::cout << mstack.size() << std::endl;
+		std::cout << mstack.size() << std::endl;
 
-	// 	mstack.push_back(3);
-	// 	mstack.push_back(5);
-	// 	mstack.push_back(737);
-	// 	mstack.push_back(0);
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		mstack.push_back(0);
 
-	// 	MutantStack<int>::iterator it = mstack.begin();
-	// 	MutantStack<int>::iterator ite = mstack.end();
+		std::list<int>::iterator it = mstack.begin();
+		std::list<int>::iterator ite = mstack.end();
 
-	// 	++it;
-	// 	--it;
-	// 	while (it != ite)
-	// 	{
-	// 		std::cout << *it << std::endl;
-	// 		++it;
-	// 	}
-
-	// 	std::cout << std::endl << "_____Using [] operator._____" << std::endl;
-
-	// 	size_t	i = 0;
-	// 	while (i < mstack.size())
-	// 	{
-	// 		std::cout << mstack[i] << std::endl;
-	// 		i++;
-	// 	}
-	// }
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
 	
 	return 0;
 }
